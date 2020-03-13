491f8bd79a8ca708a9b64c1fe6fb61b86e5dd681
RinZ26 <ivs1996@mail.ru>
2020-03-13 12:05:23 +0500

    1.3. Вызов метода с аргументами.[#248447]

---
+++ src/main/java/ru/job4j/oop/DummyDic.java
@@ -0,0 +1,19 @@
+package ru.job4j.oop;
+
+/**
+ * Класс Google Translate
+ *
+ * @author RinZ26
+ */
+public class DummyDic {
+
+    /**
+     * Переводчик
+     *
+     * @param eng запрос на английском
+     * @return потрясающий перевод
+     */
+    public String engToRus(String eng) {
+	return "Неизвестное слово." + eng;
+    }
+}
