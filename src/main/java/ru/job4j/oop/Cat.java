6cf591ea839d43a832c115825fbb74c3d6fcda92
RinZ26 <ivs1996@mail.ru>
2020-03-13 12:05:23 +0500

    1.4. Вызов метод с возвращаемым типом.[#248449]

--- src/main/java/ru/job4j/oop/Cat.java
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -6,6 +6,15 @@ package ru.job4j.oop;
  * @author RinZ26
  */
 public class Cat {
+    /**
+     * Мяу-мяу
+     *
+     * @return мяу
+     */
+    public String sound() {
+	return "may-may";
+    }
+
     public static void main(String[] args) {
 	Cat peppy = new Cat();
 	Cat sparky = new Cat();
