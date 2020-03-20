3dbf975b832e6eda71c8dc968653dbf7de519e46
RinZ26 <ivs1996@mail.ru>
2020-03-20 09:23:31 +0500

    9.1 Рефакторинг теста @Before @After[#248402]

--- src/test/java/ru/job4j/strategy/PaintTest.java
+++ src/test/java/ru/job4j/strategy/PaintTest.java
@@ -1,6 +1,8 @@
 package ru.job4j.strategy;
 
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.ByteArrayOutputStream;
@@ -12,25 +14,48 @@ import static org.junit.Assert.assertThat;
 import java.util.StringJoiner;
 
 public class PaintTest {
+    /**
+     * поле содержит дефолтный вывод в консоль.
+     */
+    private final PrintStream stdout = System.out;
+    /**
+     * буфер для хранения результата.
+     */
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+
+    /**
+     * Смена потока ввода
+     */
+    @Before
+    public void loadOutput() {
+        System.out.println("execute before method");
+        System.setOut(new PrintStream(this.out));
+    }
+
+    /**
+     * Возврат потока на дефолтный
+     */
+    @After
+    public void backOutput() {
+        System.setOut(this.stdout);
+        System.out.println("execute after method " + this.out.toString());
+    }
+
     @Test
     public void whenDrawSquare() {
-	// получаем ссылку на стандартный вывод в консоль.
-	PrintStream stdout = System.out;
-	// Создаем буфур для хранения вывода.
-	ByteArrayOutputStream out = new ByteArrayOutputStream();
-	//Заменяем стандартный вывод на вывод в пямять для тестирования.
-	System.setOut(new PrintStream(out));
-	// выполняем действия пишушиее в консоль.
-	new Paint().draw(new Square());
-	// проверяем результат вычисления
-	Square square = new Square();
-	int size = 8;
-	StringJoiner sj = new StringJoiner(System.lineSeparator());
-	for (int c = 0; c < size; c++) {
-	    sj.add("***************");
-	}
-	Assert.assertThat(square.draw(), is(sj.toString()));
-	// возвращаем обратно стандартный вывод в консоль.
-	System.setOut(stdout);
+        new Paint().draw(new Square());
+        Square square = new Square();
+        StringJoiner sj = new StringJoiner(System.lineSeparator());
+        Assert.assertThat(square.draw(), is(sj.add("***************").add("***************").add("***************").add("***************")
+                .add("***************").add("***************").add("***************").add("***************").toString()));
+    }
+
+    @Test
+    public void whenDrawTriangle() {
+        new Paint().draw(new Triangle());
+        Triangle triangle = new Triangle();
+        StringJoiner sj = new StringJoiner(System.lineSeparator());
+        Assert.assertThat(triangle.draw(), is(sj.add("   *").add("  ***").add(" *****").add("*******").toString()));
     }
 }
--- src/test/java/ru/job4j/strategy/TriangleTest.java
+++ src/test/java/ru/job4j/strategy/TriangleTest.java
@@ -10,7 +10,7 @@ import static org.hamcrest.core.Is.is;
 
 public class TriangleTest {
     @Test
-    public void whenDrawSquare() {
+    public void whenDrawTriangle() {
 	Triangle triangle = new Triangle();
 	StringJoiner sj = new StringJoiner(System.lineSeparator());
 	Assert.assertThat(triangle.draw(), is(sj.add("   *").add("  ***").add(" *****").add("*******").toString()));
