ddc4af2c2a864d7482da8e59e18e99a047c74628
RinZ26 <ivs1996@mail.ru>
2020-04-06 09:41:05 +0500

    1. Обеспечить бесперебойную работу приложения Tracker.[#248429]

--- src/main/java/ru/job4j/tracker/StubInput.java
+++ src/main/java/ru/job4j/tracker/StubInput.java
@@ -6,47 +6,32 @@ package ru.job4j.tracker;
  * @author RinZ26
  */
 public class StubInput implements Input {
-	private String[] answers;
-	private int position = 0;
+    private String[] answers;
+    private int position = 0;
 
-	public StubInput(String[] answers) {
-		this.answers = answers;
-	}
+    public StubInput(String[] answers) {
+        this.answers = answers;
+    }
 
-	/**
-	 * Перегруженный метод, для проверки, что мы не вышли за предел доступных значений
-	 *
-	 * @param question ~
-	 * @param max      ~
-	 * @return ~
-	 */
-	@Override
-	public int askInt(String question, int max) {
-		int value = -1;
-			try {
-				value = Integer.parseInt(askStr(question), max);
-			} catch (IllegalStateException ils) {
-				System.out.println(ils.getMessage());
-				System.out.println("Please select key from menu.");
-			} catch (NumberFormatException nfe) {
-				System.out.println("Please enter validate data again.");
-			}
-		return value;
-	}
+    /**
+     * Перегруженный метод, для проверки, что мы не вышли за предел доступных значений
+     *
+     * @param question ~
+     * @param max      ~
+     * @return ~
+     */
+    @Override
+    public int askInt(String question, int max) {
+		return Integer.parseInt(askStr(question), max);
+    }
 
-	@Override
-	public String askStr(String question) {
-		return answers[position++];
-	}
+    @Override
+    public String askStr(String question) {
+        return answers[position++];
+    }
 
-	@Override
-	public int askInt(String question) {
-		int value = -1;
-			try {
-				value = Integer.parseInt(askStr(question));
-			} catch (NumberFormatException nfe) {
-				System.out.println("Please enter validate data again.");
-			}
-		return value;
-	}
+    @Override
+    public int askInt(String question) {
+        return Integer.parseInt(askStr(question));
+    }
 }
