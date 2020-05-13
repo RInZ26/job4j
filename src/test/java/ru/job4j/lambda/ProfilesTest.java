2f0e2978aa60c089300d8c3be8a8cd61a50fb69c
RinZ26 <ivs1996@mail.ru>
2020-05-13 16:51:03 +0500

    3. Уникальность элементов и сортировка.[#248248]

--- src/test/java/ru/job4j/lambda/ProfilesTest.java
+++ src/test/java/ru/job4j/lambda/ProfilesTest.java
@@ -2,8 +2,7 @@ package ru.job4j.lambda;
 
 import org.junit.Test;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
@@ -11,9 +10,20 @@ import static org.junit.Assert.assertTrue;
 
 public class ProfilesTest {
     @Test
-    public void simpleList(){
+    public void simpleList() {
 	List<Profile> listOfProflies = Arrays.asList(new Profile(new Address("Yekat")), new Profile(new Address("Moscow")), new Profile(new Address("Minsk")));
 	List<Address> actual = Profiles.collect(listOfProflies);
 	assertTrue(actual.contains(new Address("Moscow")));
+    }
+
+    @Test
+    public void hardTestWithSameAddresses() {
+	List<Profile> listOfProflies = Arrays.asList(new Profile(new Address("Yekat")), new Profile(new Address("Moscow")), new Profile(new Address("Minsk")),
+		new Profile(new Address("Moscow")), new Profile(new Address("Moscow")), new Profile(new Address("Minsk")), new Profile(new Address("Moscow")));
+	List<Address> actual = Profiles.collect(listOfProflies);
+	List<Address> expected = new ArrayList(Arrays.asList(new Address("Yekat"), new Address("Moscow"), new Address("Minsk")));
+	Comparator<Address> cmpByCity = (first, second) -> first.getCity().compareTo(second.getCity());
+	Collections.sort(expected, cmpByCity);
+	assertThat(actual, is(expected));
     }
 }
