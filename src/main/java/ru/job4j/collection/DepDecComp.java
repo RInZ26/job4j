22f21942568f099a4e24b4f736f2b2892a959bb1
RinZ26 <ivs1996@mail.ru>
2020-05-18 11:30:50 +0500

    Отсортировать департаменты[#248365] Исправления

--- src/main/java/ru/job4j/collection/DepDecComp.java
+++ src/main/java/ru/job4j/collection/DepDecComp.java
@@ -9,11 +9,8 @@ import java.util.Comparator;
  */
 public class DepDecComp implements Comparator<String> {
     /**
-     * Сначала нужно проверить, что строка сплитуется
-     * Если нет, значит это одиночный K  и мы его кидаем в неотсплитованный массив
-     * Потом просто сортировка обычная
      *
-     * @param first первая строка
+     * @param first  первая строка
      * @param second вторая строка
      * @return результат сравнения
      */
@@ -21,8 +18,7 @@ public class DepDecComp implements Comparator<String> {
     public int compare(String first, String second) {
 	String[] splittedFirst = first.split("/");
 	String[] splittedSecond = second.split("/");
-	String firstPartOfO1 = splittedFirst.length == 0 ? first : splittedFirst[0];
-	String secondPartOfO1 = splittedSecond.length == 0 ? second : splittedSecond[0];
-	return secondPartOfO1.compareTo(firstPartOfO1) == 0 ? first.compareTo(second) : second.compareTo(first);
+	int result = splittedSecond[0].compareTo(splittedFirst[0]);
+	return result == 0 ? first.compareTo(second) : result;
     }
 }
